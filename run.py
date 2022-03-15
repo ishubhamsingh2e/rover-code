fbed6bec7696637bbc528c50d55d883b48ba25f0
AYAN-AMBESH <ambesh12k@gmail.com>
2022-03-15 20:32:36 +0530

    Script

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 import os
 import sys
-
+import time
 if __name__ == "__main__":
-    _inputs = sys.argv[1:]
+    _lists = sys.argv
+    _inputs = _lists[2:]
+    _filename = _lists[1]
     path = _inputs[0]
+    listimg=[]
+    for files in os.listdir(path):
+        if files.endswith(".jpg"):
+            listimg.append(str(path+files))
+    strofimg= ' '.join(map(str,listimg))
+    start=time.time()
+    os.system(f"python3 Stitch.py {strofimg}")
+    end=time.time()
+    with open(f"logs/{_filename}.csv",'a') as f:
+        f.write(f"{end-start}\n")
